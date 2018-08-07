0e4a0c51fac67060af26782ec624f5cfc2d8aafe
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-07 23:17:50 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,9 +3,10 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
   
-def initialize(name)
-  @name= name
-  @balance = 1000
-  @status = "open"
+  def initialize(name)
+    @name= name
+    @balance = 1000
+    @status = "open"
   end
+  
 end
