1037c3dcbd585877cf21d4e32e0ebe3f80322605
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-07 23:57:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? &&
+    if valid? && receiver
       @sender.balance -= amount
       @receiver.balance += amount
       @status= "complete"
