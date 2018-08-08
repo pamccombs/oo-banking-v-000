738762e46e5028de97e4779eba01840f579b3baa
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-08 00:12:34 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if valid? && @sender.balance > amount && @status == "completed"
+    if valid? && @.balance > amount && @status == "completed"
       @sender.balance += amount
       @receiver.balance -= amount
       @status= "reversed"
