90290a5ab5695c3ccb24dda55795a8ed704ce76c
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-08 00:03:42 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @receiver.balance += amount
       @status= "complete"
     else
-      "Transaction rejected. Please check your account balance"
+      @status = "Transaction rejected. Please check your account balance"
       
     end
   end
